ed6db19c38642dbfb3da864aa42409aa78966b7e
Yichi Yang <yichiyan@usc.edu>
2020-03-05 10:35:08 -0800

    Update setting

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -164,6 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = config('STATIC_ROOT')
 
 # USC Schedule of Class scraping settings
 
